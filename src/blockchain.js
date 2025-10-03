530c17529acdf7af0fc6e1495e3ca66417622bdf
Anaid0 <vpdiana06@gmail.com>
2025-10-03 12:14:46 -0500

    created code blockchain

---
+++ src/blockchain.js
@@ -0,0 +1,12 @@
+const Block = require('./block.js');
+
+class Blockchain {
+    constructor() {
+        this.chain = [];
+        this.height = -1;
+        
+    }
+}
+
+module.exports = Blockchain;
+
